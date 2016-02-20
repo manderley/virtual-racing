ad34ef6cdfcb2d9cc1bcf996b57d582a2e89a235
Rebecca Edwards <rebecca.edwards@gmail.com>
2016-02-20 22:05:45 +0100

    add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var cssImport = require('postcss-import');
 var cssnext = require('cssnext');
+var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
   var processors = [
@@ -16,12 +17,18 @@ gulp.task('styles', function() {
 
   return gulp.src('./css/main.css')
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./dest/css'));
+    .pipe(gulp.dest('./dest/css'))
+    .pipe(browserSync.stream());
 
 });
 
 gulp.task('watch', function() {
-	gulp.watch('css/*.css', ['styles']);
+
+  browserSync.init({
+    server: "./"
+  });
+
+	gulp.watch('css/*.css', ['styles']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['styles', 'watch']);
